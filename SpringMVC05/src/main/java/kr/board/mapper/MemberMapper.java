cbb0a4104b9a90adc7faf4bd71f5dbcc77bfd132
doihchiro <doihchiro@gmail.com>
2024-07-28 21:26:31 +0900

    회원 권한 추가

---
+++ SpringMVC05/src/main/java/kr/board/mapper/MemberMapper.java
@@ -0,0 +1,22 @@
+package kr.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import kr.board.entity.AuthVO;
+import kr.board.entity.Board;
+import kr.board.entity.Member;
+
+@Mapper
+public interface MemberMapper {
+	Member registerCheck(String memID);
+	int register(Member member); // 회원등록(성공1, 실패0)
+	Member memLogin(Member member);
+	int memUpdate(Member member);// 로그인 체크
+	Member getMember(String memID);
+	void memProfileUpdate(Member member);
+	void authInsert(AuthVO saveVO);
+	void authDelete(String memID);
+}
