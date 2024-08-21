2e475c9eec8781bd9533e11310297290d7f52fb8
doihchiro <doihchiro@gmail.com>
2024-08-21 19:59:24 +0900

    게시판 페이징 처리 구현

---
+++ SpringMVC08/src/main/java/kr/board/entity/Criteria.java
@@ -0,0 +1,21 @@
+package kr.board.entity;
+
+import lombok.Data;
+
+@Data
+public class Criteria {
+	
+	private int page;		// 현재 페이지 번호
+	private int perPageNum; // 한페이지에 보여줄 게시글의 수
+
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 5; // 조정
+	}
+	
+	// 현재 페이지의 게시글 인덱스 번호를 구하는 메서드
+	public int getPageStart() {			// 1page  2page  3page
+		return (page - 1) * perPageNum; // 0~     10~    20~   : limit ${pageStart},#{perPageNum}
+	}
+	
+}
